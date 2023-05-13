1f664916d02c0324bc334e88149d4f378211a96d
Priyasha <you@example.com>
2023-05-13 21:37:48 +0530

    commit

--- star_wars/App.js
+++ star_wars/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ReactDOM from "react-dom/client";
 import Header from './src/components/header/Header';
 
 import './src/styles/index.scss'
 import Body from './src/components/Body/Body';
 
-const MyApp = () => (
-    <div className='my_App'>
-     <Header/>
-     <Body/>
+const MyApp = () => {
+    const[toggle ,setToggle]=  useState(false)
+    const clickhandlder = () =>{
+        setToggle((prev) => !prev)
+    }
+ return( <div className='my_App'>
+     <Header clickhandlder={clickhandlder}/>
+   {toggle &&  <Body/>}
      
-    </div>
-  );
+    </div>);
+  }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<MyApp/>);
--- star_wars/src/components/header/Header.js
+++ star_wars/src/components/header/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 import stars  from '../../assests/images/sw_logo.png';
 import './styles.scss'
 import Content from './Content';
-const Header = () => {
+const Header = ({clickhandlder}) => {
   return (
     <div  className='stars_header'>
         <img src={stars} className='stars_header_imgs' />
         <Content />
-        <button className='stars_header_btn-ship' >Get Start ship</button>
+        <button className='stars_header_btn-ship' onClick={clickhandlder}>Get Start ship</button>
     </div>
   )
 }
