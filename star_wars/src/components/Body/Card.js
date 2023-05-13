d28b8410ea9f6c492f5948c5c1074dda9a6b6766
Priyasha <you@example.com>
2023-05-13 20:51:35 +0530

    commit

--- star_wars/src/components/Body/Card.js
+++ star_wars/src/components/Body/Card.js
@@ -8,16 +8,21 @@ const Card = () => {
     let data = await axios("https://swapi.dev/api/starships/?format=json").then(
       (data) => data.data.results
     ); 
+   
     setStarShipData(data.filter(val => val.crew <=10));
+   let s1 = starShipData.map((val) => val.crew).sort((a,b) =>a-b)
+   console.log(s1)
   };
+  
   useEffect(() => {
     getData();
-  });
+   
+  },[]);
 
   return (
     <>
-      {starShipData.map((val) => (
-        <div className="cards">
+      {starShipData.map((val, index) => (
+        <div key={`card_${index}`} className="cards">
           <div className="cards_cnt">
             <h2>{val.name}</h2>
             <div className="cards_row">
